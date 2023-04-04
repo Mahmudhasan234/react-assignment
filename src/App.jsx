62498c33ea43c9d15d131e51c27a50f251f421ea
unknown <sumaiya.rrashid@gmail.comm>
2023-04-04 16:25:37 +0200

    react toast done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 import SideCart from './components/SIdeCart/SideCart'
 import Cards from './components/navbar/Cards/Cards'
 import Navbar from './components/navbar/Navbar'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [readTime, setreadTime] = useState("")
@@ -19,13 +21,19 @@ const totalReadTime =(time) => {
     setreadTime(time)
   }
 }
+
 const [bookMark, setBookMark]= useState("")
 const setTittleBookmark = (title) => {
 let previousBookmark = JSON.parse(localStorage.getItem('bookMark'))
 
 if(previousBookmark){
+  if (previousBookmark.includes(title)) {
+    toast.error('Bookmark already exists!');
+    return;
+  }
 const newBookmark= localStorage.setItem('bookMark',JSON.stringify([title,...previousBookmark]))
 setBookMark(newBookmark)
+  
 }
 else{
   localStorage.setItem('bookMark',JSON.stringify([title]))
@@ -34,6 +42,7 @@ else{
 }
 return (
     <div className="App">
+       <ToastContainer />
      <div>
      <Navbar></Navbar>
      </div>
